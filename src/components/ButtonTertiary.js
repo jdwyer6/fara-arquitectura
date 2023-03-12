a2360c4fb7ff7bd628495c2f330d54c998b2522a
kegcheck <65734212+kegcheck@users.noreply.github.com>
2023-03-12 16:21:25 -0500

    finish interiors page

--- src/components/ButtonTertiary.js
+++ src/components/ButtonTertiary.js
@@ -1,6 +1,6 @@
 const ButtonTertiary = ({text, color}) => {
     return ( 
-        <button style={{width: 'clamp(40px, 50%, 400px)'}} className={`bg-transparent border border-${color} px-8 text-xs md:text-sm text-${color} font-medium tracking-widest py-6 rounded-xl md:mx-8 hover:text-gray-dark hover:bg-white ease-out duration-300 my-1`}>{text}</button>
+        <button style={{width: 'clamp(40px, 50%, 400px)'}} className={`bg-transparent border border-${color} px-8 text-xs md:text-sm text-${color} font-medium tracking-widest py-6 rounded-xl hover:text-gray-dark hover:bg-ivory ease-out duration-300 my-1`}>{text}</button>
      );
 }
  
--- src/pages/InterioresHomePage.js
+++ src/pages/InterioresHomePage.js
@@ -1,4 +1,9 @@
 import mainInteriorsImage from '../assets/images/new/interior_home_main.jpg';
+import aboutUsInteriorsImage from '../assets/images/new/interior_home_aboutUs.jpg';
+import galleryImage1 from '../assets/images/new/interior_home_gallery.jpg';
+import galleryImage2 from '../assets/images/new/interior_home_gallery_2.jpg';
+import galleryImage3 from '../assets/images/new/interior_home_gallery_3.jpg';
+import galleryImage4 from '../assets/images/new/interior_home_gallery_4.jpg';
 import ButtonPrimary from '../components/ButtonPrimary';
 import ButtonSecondary from '../components/ButtonSecondary';
 import ButtonTertiary from '../components/ButtonTertiary';
@@ -6,7 +11,7 @@ import ButtonTertiary from '../components/ButtonTertiary';
 const InterioresHomePage = () => {
     return ( 
         <div class="container flex flex-wrap mx-auto">
-            <section className='flex w-screen'>
+            <section className='flex w-screen mb-32'>
                 <div className='relative flex-1 overflow-hidden' style={{height:'920px'}}>
                     <img className='absolute' style={{bottom:'-80px'}} src={mainInteriorsImage}/>
                 </div>
@@ -21,17 +26,59 @@ const InterioresHomePage = () => {
                 </div>
             </section>
 
-            <section>
-                <img />
-                <div className='w-3/4 bg-brown text-white'>
-                    <h5>¿QUIÉNES SOMOS?</h5>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum aliquam id dolore sequi numquam iure nemo illum laborum fuga accusamus voluptatum, tempore quo qui ea ducimus, atque ratione, animi adipisci.</p>
-                    <ButtonTertiary text='APRENDER MÁS' color='white'/>
+            <section className='relative w-full' style={{height: '920px'}}>
+                <div class='flex h-4/5 items-center overflow-hidden'>
+                    <img src={aboutUsInteriorsImage} className='w-1/2 object-fill z-10'/>
+                    <div className='z-10 text-white p-16'>
+                        <div className='mb-16'>
+                            <p className='font-bold'>¿QUIÉNES SOMOS?</p>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum aliquam id dolore sequi numquam iure nemo illum laborum fuga accusamus voluptatum, tempore quo qui ea ducimus, atque ratione, animi adipisci. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum aliquam id dolore sequi numquam iure nemo illum laborum fuga accusamus voluptatum, tempore quo qui ea ducimus, atque ratione, animi adipisci.</p>
+                        </div>
+
+                        <ButtonTertiary text='APRENDER MÁS' color='white'/>
+                    </div>
+                </div>
+
+
+                <div className='bg-brown text-white absolute right-0 top-20 w-2/3 h-4/5'></div>
+            </section>
+
+            <section className='flex mb-32'>
+                <div className='py-16 pr-16 h-full flex flex-col justify-between'>
+                    <h1>CONOCEMOS EL PROCESO</h1>
+                    <p>El estudio planifica y coordina los trabajos de construcción directamente para garantizar los más altos estándares de proyecto, cuidando cada aspecto arquitectónico: desde los estudios y evaluaciones preliminares hasta la planificación ejecutiva y las tecnologías de construcción, hasta el mobiliario de sastrería.</p>
+                    <ButtonTertiary text='NUESTROS PROYECTOS'/>
+                </div>
+                <div className='py-16 pl-16'>
+                    <div className='mb-16'>
+                        <h2 className='mb-8'>EXPERIENCIA SIN IGUAL</h2>
+                        <p>Con Farah Arquitectura, no hay retos ni contratiempos, sino oportunidades de diseño. Cualquier reto contextual es una gran oportunidad creativa para el desarrollo integral y potencial de proyecto, desde su desarrollo conceptual hasta su ejecución.</p>
+                    </div>
+                    <div>
+                        <h2 className='mb-8'>EQUIPO MULTIDISCIPLINARIO</h2>
+                        <p>Fara Arquitectura ofrece un desarrollo de experiencias hechas a la medida de cada cliente con la participación coordinada de su equipo interdisciplinario especializado. </p>
+                    </div>
+
                 </div>
             </section>
 
+            <section className='flex flex-wrap mb-32'>
 
-            
+                <img src={galleryImage1} className='w-1/3 pr-8 py-8'/>
+                <img src={galleryImage2} className='w-2/3 pl-8 py-8'/>
+                <img src={galleryImage3} className='w-2/3 pr-8 py-8'/>
+                <img src={galleryImage4} className='w-1/3 pl-8 py-8'/>
+            </section>
+
+
+            <section>
+                <div className='text-center'>
+                    <strong>CONTÁCTANOS HOY</strong>
+                    <p className='mb-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum officiis at aspernatur fugiat aliquam, impedit veritatis deserunt laboriosam repellendus quibusdam deleniti sed dicta ex nam, voluptate enim tenetur quasi. Aperiam?</p>
+                    <ButtonSecondary text='CONTACTO'/>
+                </div>
+
+            </section>
 
 
 
@@ -41,6 +88,7 @@ const InterioresHomePage = () => {
         </div>
      );
      //Replace with a component
+     //TODO: Add footer
 }
  
 export default InterioresHomePage;
